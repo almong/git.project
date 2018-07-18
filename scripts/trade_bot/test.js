f6d15d88b59d9e3b32b43478a094d7735442d4af
almong <admin@ag-net.ru>
2018-07-18 17:11:21 +0300

    bot

---
+++ scripts/trade_bot/test.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws')
+
+const wss = new WebSocket('wss://api.bitfinex.com/ws/')
+wss.onmessage = (msg) => {
+    console.log(msg.data);
+    var data=JSON.parse(msg.data);
+    JSON.stringify(data);
+    if (data.event ==='info'){
+        console.log('Подключение установленно, версия сервера '+data.version);
+    
+    }
+    if (data.event ==='subscribed'){
+        console.log('Подписка на трейд в паре '+data.pair+ ' оформлена');
+        
+    }
+}
+wss.onopen = () => {
+    var obj = {
+        "event": "subscribe",
+        "channel": "trades",
+        "pair": "BTCUSD"
+    }
+    wss.send(JSON.stringify(obj));
+  // API keys setup here (See "Authenticated Channels")
+}
+
